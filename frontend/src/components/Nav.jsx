a1a2001b42fc8b7b4b33838f1a4b6089a1a94a74
Rekia Mansour Ouahchia <rmansourouahchia@gmail.com>
2025-08-12 23:11:25 +0100

    hero DZGN + mdf nav

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -10,7 +10,7 @@ function Nav() {
   ]
 
   return (
-    <header className='padding-x py-6 z-10 absolute w-full'>
+    <header className='padding-x pt-4 pb-4 z-10 w-full bg-white'>
       <nav className='flex flex-row justify-between items-center max-container '>
         <a 
         className='flex flex-row justify-center items-center'
@@ -21,13 +21,13 @@ function Nav() {
           width={45}
           height={45}
           />
-          <h3 className='playfair-display-400 text-4xl text-[#474747]'>Bistro Bliss</h3>
+          <h3 className='playfair-display-400 text-3xl text-[#474747]'>Bistro Bliss</h3>
         </a>
 
         <ul className='flex felx-row flex-1 justify-center items-center gap-x-2 text-oliv-07 dm-mono-medium font-semibold max-lg:hidden'>
           {navLinks.map((link,i)=>(
             <li 
-            className='hover:bg-[#DBDFD0] flex justify-center items-center transition duration-300 ease-in-out py-0.5 px-3 rounded-4xl'
+            className='hover:bg-[#DBDFD0] flex justify-center items-center transition duration-300 ease-in-out text-[14px] py-1 px-4 rounded-4xl'
             key={i}>
               <a href={link.href}>
                 {link.label}
@@ -36,7 +36,7 @@ function Nav() {
           ))}
         </ul>
 
-          <button className='font-semibold text-oliv-07 border-oliv-07 border-2 py-2 px-4 rounded-4xl hover:text-white dm-mono-medium hover:bg-oliv-07 transition duration-300 ease-in-out'>
+          <button className='font-semibold text-oliv-07 border-oliv-07 border-2 py-2 px-4 rounded-4xl hover:text-white text-[14px] dm-mono-medium hover:bg-oliv-07 active:opacity-80 transition duration-300 ease-in-out cursor-pointer'>
             Sign up
           </button>
 
--- frontend/src/sections/Hero.jsx
+++ frontend/src/sections/Hero.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import bg from '../assets/images/bg.png';
 
 function Hero() {
   return (
-    <div></div>
+    <section id="hero" 
+    className='w-full  max-container bg-cover bg-center h-[90vh] w-screen flex justify-center items-center' 
+    style={{ backgroundImage: `url(${bg})` }}>
+
+      <div className="flex flex-col text-center justify-center item-center gap-5">
+
+        <h1 className='playfair-display-400 leading-none text-oliv-07 max-lg:text-[75px] max-sm:text-[60px] text-[90px] '>Best food for <br /> your taste</h1>
+
+        <p className='text-oliv-07 '>Discover delectable cuisine and unforgettable moments <br /> in our welcoming, culinary haven.</p>
+
+        <div>
+          <button className='bg-rouge-brick text-white px-6 py-4 hover:opacity-80 active:opacity-100 text-[12px] font-semibold rounded-4xl transition duration-300 ease-in-out cursor-pointer'>
+          Explore Menu
+        </button>
+        </div>
+        
+      
+      </div>
+    </section>
   )
 }
 
